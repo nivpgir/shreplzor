c17b059ee8f95f816660bd67309cbf721d7d4a67
nivpgir <Nivpgir@gmail.com>
2021-12-10 01:22:50 +0200

    blablabla just playing around

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,32 @@
+use std::collections::BTreeSet;
+
 use eframe::{egui, epi};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
+#[derive(Default)]
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
-    value: f32,
+    code: String,
+    displayed_output: String,
+    output_history: Vec<HistoryItem>,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
-    }
+#[derive(Default, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct HistoryItem{
+    code: String,
+    output: String,
 }
+// impl Default for TemplateApp {
+//     fn default() -> Self {
+//         Self {
+//             // Example stuff:
+//             label: "Hello World!".to_owned(),
+// 	    code: "".to_owned(),
+//             value: 2.7,
+//         }
+//     }
+// }
 
 impl epi::App for TemplateApp {
     fn name(&self) -> &str {
@@ -52,12 +58,6 @@ impl epi::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -70,49 +70,39 @@ impl epi::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
+        egui::SidePanel::left("history").show(ctx, |ui| {
+            ui.heading("History");
+	    for (ind, hist_item) in self.output_history.iter().enumerate() {
+		ui.collapsing(
+		    format!("({}) {}", ind, hist_item.code),
+		    |ui| { ui.label(&hist_item.output); }
+		);
+		ui.separator();
+	    }
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
+            ui.heading("Your code here:");
+	    ui.code_editor(&mut self.code);
+	    let prev = self.output_history.first().cloned();
+	    if ui.button("Execute").clicked() {
+		let output = "output".to_string();
+		if let Some(prev_item) = prev {
+		    if self.code != prev_item.code{
+			self.output_history
+			    .insert(0,
+				    HistoryItem{ code: self.code.clone(), output: output.clone() }
+			);
+		    }
+		} else {
+		    self.output_history
+			.insert(0,
+				HistoryItem{ code: self.code.clone(), output: output.clone() }
+			);
+		}
+		self.displayed_output = self.code.clone();
+		ui.monospace(output);
+	    }
+	});
     }
 }
